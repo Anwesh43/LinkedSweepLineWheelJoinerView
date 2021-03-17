345b06edf0006401750a33b3083caa198fb63c40
Anwesh43 <anweshthecool0@gmail.com>
2021-03-18 00:43:10 +0530

    adding SweepLineWheelJoinerView class

--- swwplinewheeljoinerview/src/main/java/com/example/swwplinewheeljoinerview/SweepLineWheelJoinerView.kt
+++ swwplinewheeljoinerview/src/main/java/com/example/swwplinewheeljoinerview/SweepLineWheelJoinerView.kt
@@ -68,3 +68,19 @@ fun Canvas.drawSLWJNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSweepWheelJoiner(scale, w, h, paint)
 }
+
+class SweepLineWheelJoinerView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
